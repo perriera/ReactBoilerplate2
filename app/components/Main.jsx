2dd277c1f6fa00c0e5e63e63141ffc3167ee33af
Perry Anderson <perry.anderson@gmail.com>
2017-01-04 03:05:32 -0330

    Initial setup

---
+++ app/components/Main.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+
+var Main = (props) => {
+    return (
+        <div>
+            <div>
+                <p>Main.jsx Rendered</p>
+                {props.children}
+            </div>
+        </div>
+    );
+};
+
+module.exports = Main;
